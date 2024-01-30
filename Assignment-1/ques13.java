9ae207b2124f04a74d38ca83c1fe09fab1bba6d4
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-01-30 12:42:00 +0530

    Add files via upload

---
+++ Assignment-1/ques13.java
@@ -0,0 +1,32 @@
+package Assigment;
+
+public class ques13 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 7};
+        int m = 6;
+
+        int result = searchInsert(arr, m);
+        System.out.println("Output: " + result);
+    }
+
+    private static int searchInsert(int[] arr, int m) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == m) {
+                return mid; // Target value found
+            } else if (arr[mid] < m) {
+                left = mid + 1; // Adjust the search range to the right
+            } else {
+                right = mid - 1; // Adjust the search range to the left
+            }
+        }
+
+        // If the target is not found, 'left' will be the position to insert the target
+        return left;
+    }
+}
+
