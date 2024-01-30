97efead64c1c4bc355ff6bc8109db55cbe344a6b
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-01-30 18:39:30 +0530

    Update ques5.java

--- Day-3/Day3/ques5.java
+++ Day-3/Day3/ques5.java
@@ -1,7 +1,48 @@
 package Day3;
 
+import java.util.Scanner;
+
 public class ques5 {
+    public static void totalSum(int[][] mat) {
+        int n = mat.length;
+        int sum =0;
+//top & bottom row
+        for(int i=0; i<n; i++) {
+            sum = sum + mat[0][i];
+            sum = sum + mat[n-1][i];
+        }
+//left & right column
+        for(int i=1; i<n-1; i++) {
+            sum = sum + mat[i][0];
+            sum = sum + mat[i][n-1];
+        }
+
+//left diagonal & right diagonal
+        for(int i=1; i<n-1; i++) {
+            sum = sum + mat[i][i];
+            sum = sum + mat[i][n-1-i];
+        }
+        if(n%2 !=0) {
+            sum = sum - mat[n/2][n/2];
+        }
+        System.out.println(sum);
+    }
+    public static int[][] take2DInput() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if (n == 0) {
+            return new int[0][0];
+        }
+        int[][] mat = new int[n][n];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                mat[row][col] = s.nextInt();
+            }
+        }
+        return mat;
+    }
     public static void main(String[] args) {
-        
+        int[][] mat = take2DInput();
+        totalSum(mat);
     }
 }
