ce6a863fdad7e10dd39dd8fb21770d60762d74f4
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-07 15:39:46 +0530

    Update ques2.java

--- Day-9/Day_9/ques2.java
+++ Day-9/Day_9/ques2.java
@@ -4,35 +4,40 @@ import java.util.ArrayList;
 
 public class ques2 {
     public static void main(String[] args) {
-        ArrayList<String>wordList = new ArrayList<>();
+        // Create an ArrayList of words
+        ArrayList<String> wordList = new ArrayList<>();
         wordList.add("cats");
         wordList.add("dog");
         wordList.add("sand");
         wordList.add("and");
         wordList.add("cat");
 
+        // Define the input string
         String inputString = "catsandog";
 
-        System.out.println(ans(wordList,inputString));//alt+enter
-
+        // Print the result of the function call
+        System.out.println(ans(wordList, inputString));
     }
 
+    // Function to determine if inputString can be segmented into words from wordList
     private static boolean ans(ArrayList<String> wordList, String inputString) {
-      if(wordList.contains(inputString))
-      {
-          return true;
-      }
-
-
-        for (int i = 0; i < inputString.length(); i++) {
-             String check =  inputString.substring(0,i);
-
-             if(wordList.contains(check))
-             {
-                 return ans(wordList,inputString.substring(i));
-             }
-
+        // If the wordList contains the entire inputString, return true
+        if (wordList.contains(inputString)) {
+            return true;
         }
+
+        // Iterate through the characters of inputString
+        for (int i = 0; i < inputString.length(); i++) {
+            // Get the substring from the beginning up to index i
+            String check = inputString.substring(0, i);
+
+            // If the wordList contains the substring
+            if (wordList.contains(check)) {
+                // Recursively call the function with the remaining substring
+                return ans(wordList, inputString.substring(i));
+            }
+        }
+        // If no segmentation is possible, return false
         return false;
     }
 }
