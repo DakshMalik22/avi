b9c0b9a3a196ef5e792efe48c464cd2efc810510
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-15 10:39:13 +0530

    Create InsufficientFundsException.java

---
+++ Day-12/bank_system/InsufficientFundsException.java
@@ -0,0 +1,6 @@
+class InsufficientFundsException extends BankException {
+    // Constructor to initialize the exception with a message
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
