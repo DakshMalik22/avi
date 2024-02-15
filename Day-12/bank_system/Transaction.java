5de2874ff0313881d532149d91ae32236555ea14
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-15 10:40:31 +0530

    Create Transaction.java

---
+++ Day-12/bank_system/Transaction.java
@@ -0,0 +1,13 @@
+class Transaction {
+    // Declare private variables for transaction ID, type, and amount
+    private int transactionId;
+    private String transactionType;
+    private int amount;
+
+    // Constructor to initialize transaction details
+    public Transaction(int transactionId, String transactionType, int amount) {
+        this.transactionId = transactionId;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+}
