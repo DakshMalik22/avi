ae58ad5970e42c1ad1e1a8e01e352537883769fb
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-15 10:44:31 +0530

    Update BankAccount.java

--- Day-12/bank_system/BankAccount.java
+++ Day-12/bank_system/BankAccount.java
@@ -10,6 +10,7 @@ class BankAccount {
 
     // Constructor to initialize account number and balance
     public BankAccount(int accountNumber, int balance) {
+
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
