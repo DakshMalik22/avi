505c6ad7d52bd13a2bc47eee0e992484a48e4dd8
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-16 22:40:19 +0530

    Update File.java

--- Day-14/Distributed_File_System/File.java
+++ Day-14/Distributed_File_System/File.java
@@ -1,27 +1,31 @@
 package Distributed_File_System;
 
+// Class representing a file in the Distributed File System (DFS)
 class File {
-    private String fileName;
-    private int fileSize;
-    private boolean isReplicated;
+    // Attributes of the file
+    private String fileName;    // Name of the file
+    private int fileSize;       // Size of the file
+    private boolean isReplicated;   // Flag indicating whether the file is replicated
 
+    // Constructor to initialize the file attributes
     public File(String fileName, int fileSize, boolean isReplicated) {
         this.fileName = fileName;
         this.fileSize = fileSize;
         this.isReplicated = isReplicated;
     }
 
+    // Method to get the size of the file
     public int getFileSize() {
         return fileSize;
     }
 
+    // Method to get the name of the file
     public String getFileName() {
         return fileName;
     }
 
+    // Method to check if the file is replicated
     public boolean isReplicated() {
         return isReplicated;
     }
-
-
 }
