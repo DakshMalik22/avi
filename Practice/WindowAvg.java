ca9f6f1a656318c04d157f2674c6ce18c57adcdb
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-28 15:19:50 +0530

    Update WindowAvg.java

--- Practice/WindowAvg.java
+++ Practice/WindowAvg.java
@@ -3,7 +3,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class q3 {
+public class WindowAvg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
