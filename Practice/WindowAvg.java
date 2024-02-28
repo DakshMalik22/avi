9eb4be74affc949825225cf30953c4d754d9f99d
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-28 15:16:02 +0530

    Update and rename q3 to WindowAvg.java

--- Practice/WindowAvg.java
+++ Practice/WindowAvg.java
@@ -6,17 +6,22 @@ import java.util.Scanner;
 public class q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // Prompting the user to input the size of the number
         System.out.print("Enter the size of the number: ");
         int n = sc.nextInt();
 
+        // ArrayList to store the digits of the number
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
+            // Prompting the user to input each digit of the number
             System.out.print("Enter digit "+(i+1)+": ");
             int digit = sc.nextInt();
             list.add(digit);
         }
 
         int window_size;
+        // Loop to ensure valid window size input
         do {
             System.out.print("Enter the window size: ");
             window_size = sc.nextInt();
@@ -27,17 +32,20 @@ public class q3 {
         int end = window_size-1;
         ArrayList<Integer> result = new ArrayList<>();
 
+        // Sliding window approach to calculate the average of each window
         while(end<n){
             double sum = 0;
             for(int i=start; i<=end; i++){
                 sum+= list.get(i);
             }
+            // Calculating the average of the current window
             int ans = (int)Math.ceil(sum/window_size);
             result.add(ans);
             start++;
             end++;
         }
 
+        // Printing the result (average of each window)
         System.out.println(result);
     }
 }
