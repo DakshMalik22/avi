66cf11049b2d280171618ba6138dbd12580080fa
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-02-28 15:22:14 +0530

    Update Singleton.java

--- Singleton/Singleton/Singleton.java
+++ Singleton/Singleton/Singleton.java
@@ -1,19 +1,24 @@
 package Singleton;
 
+// Singleton class definition
 public class Singleton {
+    // Private constructor to prevent instantiation from outside the class
     private Singleton() {
-
+        // Constructor body is empty
     }
 
+    // Static variable to hold the single instance of the class
     private static Singleton instance;
 
+    // Method to get the instance of the Singleton class
     public static Singleton getInstance() {
+        // Check if the instance is null (i.e., not instantiated yet)
         if (instance == null) {
+            // Create a new instance if it doesn't exist
             instance = new Singleton();
         }
 
+        // Return the instance
         return instance;
-
     }
-
 }
