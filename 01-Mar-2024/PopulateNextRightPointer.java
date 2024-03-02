9cae98753054589a8010b12e043ba02e153e494b
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-03-02 12:38:05 +0530

    Update PopulateNextRightPointer.java

--- 01-Mar-2024/PopulateNextRightPointer.java
+++ 01-Mar-2024/PopulateNextRightPointer.java
@@ -15,7 +15,7 @@ public class PopulateNextRightPointer {
      * Populates each next pointer to point to its next right node.
      * If there is no next right node, the next pointer should be set to NULL.
      * 
-     * @param root The root of the binary tree
+     
      */
     public void connect(TreeLinkNode root) throws IllegalArgumentException {
         // Check if the root is null
