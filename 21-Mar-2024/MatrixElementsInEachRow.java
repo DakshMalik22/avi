2b04ecab143e479fd6c98726a0b3e0a9f0bdb78d
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:41:06 +0530

    Update MatrixElementsInEachRow.java

--- 21-Mar-2024/MatrixElementsInEachRow.java
+++ 21-Mar-2024/MatrixElementsInEachRow.java
@@ -3,16 +3,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class MatrixElementsInEachRow {
+public class CommonElementsInMatrixRows {
     public static void main(String[] args) {
-        int[][] matrix1 = {{1, 4, 5, 6}, {3, 4, 5, 6}, {5, 6, 7, 2}};
-        int[][] matrix2 = {{4, 6}, {6, 4}, {2, 6}};
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{2, 4, 6}, {3, 5, 7}, {1, 8, 9}};
 
         List<Integer> result1 = findCommonElements(matrix1);
         List<Integer> result2 = findCommonElements(matrix2);
 
-        System.out.println(result1); // Output: [5, 6]
-        System.out.println(result2); // Output: [6]
+        System.out.println("Common elements in matrix1: " + result1); // Output: [2]
+        System.out.println("Common elements in matrix2: " + result2); // Output: []
     }
 
     public static List<Integer> findCommonElements(int[][] matrix) {
@@ -20,15 +20,15 @@ public class MatrixElementsInEachRow {
         Set<Integer> commonElements = new HashSet<>();
 
         // Add elements of the first row to the set
-        for (int j = 0; j < matrix[0].length; j++) {
-            commonElements.add(matrix[0][j]);
+        for (int element : matrix[0]) {
+            commonElements.add(element);
         }
 
         // Iterate through each subsequent row and find intersection with the set
         for (int i = 1; i < rows; i++) {
             Set<Integer> currentRowElements = new HashSet<>();
-            for (int j = 0; j < matrix[i].length; j++) {
-                currentRowElements.add(matrix[i][j]);
+            for (int element : matrix[i]) {
+                currentRowElements.add(element);
             }
             // Take intersection of the current row elements with the commonElements set
             commonElements.retainAll(currentRowElements);
