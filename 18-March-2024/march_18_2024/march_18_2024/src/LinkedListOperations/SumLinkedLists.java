41c5cacf054c567db2e9a611ea1c9b39a6a52424
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 23:49:00 +0530

    Update SumLinkedLists.java

--- 18-March-2024/march_18_2024/march_18_2024/src/LinkedListOperations/SumLinkedLists.java
+++ 18-March-2024/march_18_2024/march_18_2024/src/LinkedListOperations/SumLinkedLists.java
@@ -1,92 +1,33 @@
-package LinkedListOperations;
-
-import java.util.Scanner;
-
 class ListNode {
     int val;
     ListNode next;
-
-    ListNode() {}
 
     ListNode(int val) {
         this.val = val;
     }
 }
 
-public class SumLinkedLists {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int sizeOfList1 = getUserInput(scanner, "Enter size of the first list:");
-        ListNode head1 = createList(scanner, sizeOfList1, "Enter elements of the first list:");
-
-        int sizeOfList2 = getUserInput(scanner, "Enter size of the second list:");
-        ListNode head2 = createList(scanner, sizeOfList2, "Enter elements of the second list:");
-
-        ListNode reversedList1 = reverseList(head1.next);
-        ListNode reversedList2 = reverseList(head2.next);
-
-        ListNode reversedAddedList = addLists(reversedList1, reversedList2);
-
-        ListNode finalAddedList = reverseList(reversedAddedList);
-        System.out.println("Result list:");
-        printList(finalAddedList);
-    }
-
-    public static int getUserInput(Scanner scanner, String message) {
-        System.out.println(message);
-        return scanner.nextInt();
-    }
-
-    public static ListNode createList(Scanner scanner, int size, String message) {
-        System.out.println(message);
-        ListNode head = new ListNode();
-        ListNode list = head;
-        for (int i = 0; i < size; i++) {
-            list.next = new ListNode(scanner.nextInt());
-            list = list.next;
-        }
-        return head;
-    }
-
-    public static void printList(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val);
-            head = head.next;
-        }
-    }
-
-    public static ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while (current != null) {
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
-    }
-
-    public static ListNode addLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode();
-        ListNode current = result;
+public class AddTwoNumbersLinkedList {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
         int carry = 0;
 
-        while (list1 != null || list2 != null || carry == 1) {
-            int sum = carry;
-            if (list1 != null) {
-                sum += list1.val;
-                list1 = list1.next;
-            }
-            if (list2 != null) {
-                sum += list2.val;
-                list2 = list2.next;
-            }
-            current.next = new ListNode(sum % 10);
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
             carry = sum / 10;
+            current.next = new ListNode(sum % 10);
             current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        return result.next;
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummy.next;
     }
 }
