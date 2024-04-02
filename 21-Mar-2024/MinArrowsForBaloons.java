cce1a012a8482db51ecf64defd58f4b6887c5636
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:41:51 +0530

    Update MinArrowsForBaloons.java

--- 21-Mar-2024/MinArrowsForBaloons.java
+++ 21-Mar-2024/MinArrowsForBaloons.java
@@ -1,24 +1,26 @@
 import java.util.Arrays;
 
-public class MinArrowsForBaloons {
+public class MinimumArrowsForBalloons {
     public static void main(String[] args) {
-        int[] ARR1 = {2, 1, 5, 4, 3};
-        int[] ARR2 = {3, 2, 1,};
-        System.out.println(minArrows(ARR1));
-        System.out.println(minArrows(ARR2));
+        int[] balloons1 = {2, 1, 5, 4, 3};
+        int[] balloons2 = {3, 2, 1};
+        System.out.println("Minimum arrows for balloons1: " + minArrows(balloons1)); // Output: 2
+        System.out.println("Minimum arrows for balloons2: " + minArrows(balloons2)); // Output: 1
     }
 
-    public static int minArrows(int[] ARR) {
-        if (ARR.length == 0) return 0;
-
+    public static int minArrows(int[] balloons) {
+        if (balloons.length == 0) return 0;
 
         int arrows = 1;
-        int maxHeight = ARR[0];
+        int maxHeight = balloons[0];
 
-        for (int i = 1; i < ARR.length; i++) {
-            if (ARR[i] > maxHeight) {
+        // Loop through the array to find the minimum number of arrows required
+        for (int i = 1; i < balloons.length; i++) {
+            // If the current balloon is above the maximum height shot so far,
+            // then a new arrow is required and update the maximum height shot
+            if (balloons[i] > maxHeight) {
                 arrows++;
-                maxHeight = ARR[i];
+                maxHeight = balloons[i];
             }
         }
 
