6eb6cf3e06c59041ac0455a90d2ab8e772db66a9
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:31:15 +0530

    Update FirstMissingPositive.java

--- march-20/FirstMissingPositive.java
+++ march-20/FirstMissingPositive.java
@@ -1,42 +1,41 @@
 import java.util.Scanner;
 
-public class FirstMissingPositive {
-    public static void main(String[] args){
+public class FindFirstMissingPositive {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Please enter the size of the array:");
         int size = scanner.nextInt();
-        int nums[] = new int[size];
-        System.out.println("Enter elements of array:");
-        for (int i = 0; i < size; i++){
-            nums[i] = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
         }
-        System.out.println("First missing positive:");
-        System.out.println(firstMissingPositive(nums));
+        System.out.println("First missing positive number:");
+        System.out.println(findFirstMissingPositive(array));
     }
 
-    public static int firstMissingPositive(int[] nums) {
-        // cycle sort
+    public static int findFirstMissingPositive(int[] nums) {
+        // Cycle sort
         int i = 0;
 
-        while(i < nums.length){
-            int correct = nums[i] - 1;
-            if(nums[i] < nums.length && nums[i] > 0 && nums[i] != nums[correct]){
-                // swap the two numbers
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] < nums.length && nums[i] > 0 && nums[i] != nums[correctIndex]) {
+                // Swap the two numbers
                 int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
-            }
-            else
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            } else
                 ++i;
         }
 
-        // now the list is sorted so find missing number
-        for(int j = 0; j < nums.length; ++j){
-            if(nums[j] != j+1)
-                return j+1;
+        // Now the array is sorted, so find the missing number
+        for (int j = 0; j < nums.length; ++j) {
+            if (nums[j] != j + 1)
+                return j + 1;
         }
 
-        //when every number is present in the array the number must be nums.length+1
-        return nums.length+1;
+        // When every number is present in the array, the missing number must be nums.length+1
+        return nums.length + 1;
     }
 }
