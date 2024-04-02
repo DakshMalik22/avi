fcbfde0bd560dfa002d6fc75bf25e17e158c7e1a
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:46:12 +0530

    Update PatternOccuranceInText.java

--- 21-Mar-2024/PatternOccuranceInText.java
+++ 21-Mar-2024/PatternOccuranceInText.java
@@ -1,65 +1,66 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class PatternOccuranceInText {
-    public static void main(String[] args) {
-        String text1 = "cxyzghxyzvjkxyz";
-        String pattern1 = "xyz";
-        printOccurrences(text1, pattern1); // Output: 2 7 13
-
-        String text2 = "ababacabab";
-        String pattern2 = "aba";
-        printOccurrences(text2, pattern2); // Output: 1 3 7
-
-        String text3 = "abcd";
-        String pattern3 = "xy";
-        printOccurrences(text3, pattern3); // Output: 0
-    }
-
-    public static void printOccurrences(String text, String pattern) {
-        List<Integer> occurrences = findOccurrences(text, pattern);
-        for (int index : occurrences) {
-            System.out.print((index + 1) + " "); // 1-indexed
-        }
-        System.out.println();
-    }
-
-    public static List<Integer> findOccurrences(String text, String pattern) {
+public class StringPatternMatching {
+    
+    public static List<Integer> findPatternOccurrences(String text, String pattern) {
         List<Integer> occurrences = new ArrayList<>();
         int n = text.length();
         int m = pattern.length();
-        int prime = 31;
-        long patternHash = calculateHash(pattern, prime);
-        long textHash = calculateHash(text.substring(0, m), prime);
-
-        for (int i = 0; i <= n - m; i++) {
-            if (patternHash == textHash && text.substring(i, i + m).equals(pattern)) {
-                occurrences.add(i);
-            }
-            if (i < n - m) {
-                textHash = recalculateHash(text, i, m, textHash, prime);
+        
+        // Construct the prefix array for the pattern
+        int[] prefixArray = new int[m];
+        int j = 0;
+        for (int i = 1; i < m; i++) {
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                j++;
+                prefixArray[i] = j;
+            } else {
+                if (j != 0) {
+                    j = prefixArray[j - 1];
+                    i--;
+                } else {
+                    prefixArray[i] = 0;
+                }
             }
         }
-
+        
+        // Perform pattern matching using the prefix array
+        int i = 0;
+        j = 0;
+        while (i < n) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+            }
+            if (j == m) {
+                occurrences.add(i - j + 1);
+                j = prefixArray[j - 1];
+            } else if (i < n && text.charAt(i) != pattern.charAt(j)) {
+                if (j != 0)
+                    j = prefixArray[j - 1];
+                else
+                    i++;
+            }
+        }
+        
         return occurrences;
     }
-
-    // method to calculate hash value of substring of text
-    public static long calculateHash(String str, int prime) {
-        long hash = 0;
-        long pPow = 1;
-        for (char c : str.toCharArray()) {
-            hash = (hash + (c - 'a' + 1) * pPow) % prime;
-            pPow = (pPow * prime) % prime;
-        }
-        return hash;
-    }
-
-    //method to calculate rolling hash
-    public static long recalculateHash(String text, int oldIndex, int patternLength, long oldHash, int prime) {
-        long newHash = oldHash - (text.charAt(oldIndex) - 'a' + 1);
-        newHash /= prime;
-        newHash += (text.charAt(oldIndex + patternLength) - 'a' + 1) * Math.pow(prime, patternLength - 1);
-        return newHash;
+    
+    public static void main(String[] args) {
+        String text1 = "cxyzghxyzvjkxyz";
+        String pattern1 = "xyz";
+        List<Integer> occurrences1 = findPatternOccurrences(text1, pattern1);
+        System.out.println(occurrences1); // Output: [2, 7, 13]
+        
+        String text2 = "ababacabab";
+        String pattern2 = "aba";
+        List<Integer> occurrences2 = findPatternOccurrences(text2, pattern2);
+        System.out.println(occurrences2); // Output: [1, 3, 7]
+        
+        String text3 = "abcd";
+        String pattern3 = "xy";
+        List<Integer> occurrences3 = findPatternOccurrences(text3, pattern3);
+        System.out.println(occurrences3); // Output: []
     }
 }
