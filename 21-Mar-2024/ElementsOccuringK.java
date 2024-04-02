dc3f727d3b6d7ec5abbf1267ee3929c93862c09b
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:37:37 +0530

    Update ElementsOccuringK.java

--- 21-Mar-2024/ElementsOccuringK.java
+++ 21-Mar-2024/ElementsOccuringK.java
@@ -3,36 +3,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ElementsOccuringK {
+public class FrequentElementsFinder {
     public static void main(String[] args) {
-        int[][] arrs = {{1, 1, 2, 1, 2, 4, 3, 4}, {1, 1, 1, 2, 2, 2}};
-        int[] ks = {4, 6};
+        int[][] arrays = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {5, 5, 5, 5, 5, 5, 6, 6, 6}};
+        int[] thresholds = {5, 3};
 
-        for (int i = 0; i < arrs.length; i++) {
-            List<Integer> result = findFrequentElements(arrs[i], ks[i]);
-            System.out.println(result);
+        for (int i = 0; i < arrays.length; i++) {
+            List<Integer> result = findFrequentElements(arrays[i], thresholds[i]);
+            System.out.println("Frequent elements for array " + (i + 1) + ": " + result);
         }
     }
 
-    public static List<Integer> findFrequentElements(int[] arr, int k) {
-        int n = arr.length;
-        int threshold = n / k;
+    public static List<Integer> findFrequentElements(int[] array, int k) {
+        int n = array.length;
+        int frequencyThreshold = n / k;
 
         Map<Integer, Integer> frequencyMap = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
+        List<Integer> frequentElements = new ArrayList<>();
 
         // Count frequency of each element
-        for (int num : arr) {
+        for (int num : array) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
         // Check if frequency is greater than or equal to threshold
         for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            if (entry.getValue() >= threshold) {
-                result.add(entry.getKey());
+            if (entry.getValue() >= frequencyThreshold) {
+                frequentElements.add(entry.getKey());
             }
         }
 
-        return result;
+        return frequentElements;
     }
 }
