430c2ceabe086573eee41ec66dea94d6dfad9d4f
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:35:39 +0530

    Update SubarraySumEqualsk.java

--- march-20/SubarraySumEqualsk.java
+++ march-20/SubarraySumEqualsk.java
@@ -1,34 +1,35 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class SubarraySumEqualsk {
-    public static void main(String[] args){
+public class SubarraySumCounter {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Please enter the size of the array:");
         int size = scanner.nextInt();
-        int nums[] = new int[size];
-        System.out.println("Enter elements of array:");
-        for (int i = 0; i < size; i++){
+        int[] nums = new int[size];
+        System.out.println("Enter elements of the array:");
+        for (int i = 0; i < size; i++) {
             nums[i] = scanner.nextInt();
         }
-        System.out.println("Enter value of k:");
+        System.out.println("Enter the value of k:");
         int k = scanner.nextInt();
-        System.out.println("count of subarrays with sum k:");
-        System.out.println(subarraySum(nums, k));
+        System.out.println("Count of subarrays with sum equal to k:");
+        System.out.println(countSubarraysWithSum(nums, k));
     }
-    public static int subarraySum(int[] nums, int k){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0,1); // identity element
-        int sum = 0;
-        int count = 0;
-        for (int i = 0; i < nums.length;++i){
-            sum += nums[i];
-            int rem = sum - k;
-            if(map.containsKey(rem)){
-                count += map.get(rem);
+
+    public static int countSubarraysWithSum(int[] nums, int k) {
+        HashMap<Integer, Integer> sumCountMap = new HashMap<>();
+        sumCountMap.put(0, 1); // Identity element
+        int currentSum = 0;
+        int totalCount = 0;
+        for (int num : nums) {
+            currentSum += num;
+            int remainder = currentSum - k;
+            if (sumCountMap.containsKey(remainder)) {
+                totalCount += sumCountMap.get(remainder);
             }
-            map.put(sum, map.getOrDefault(sum,0)+1);
+            sumCountMap.put(currentSum, sumCountMap.getOrDefault(currentSum, 0) + 1);
         }
-        return count;
+        return totalCount;
     }
 }
