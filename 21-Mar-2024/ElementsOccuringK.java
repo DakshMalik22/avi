14a4c6957efb0e2ac4abdba8c341f3cf7630d85a
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:56:46 +0530

    Update ElementsOccuringK.java

--- 21-Mar-2024/ElementsOccuringK.java
+++ 21-Mar-2024/ElementsOccuringK.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FrequentElementsFinder {
+public class ElementsOccuringK {
     public static void main(String[] args) {
         int[][] arrays = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {5, 5, 5, 5, 5, 5, 6, 6, 6}};
         int[] thresholds = {5, 3};
