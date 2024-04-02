c39baa7460e8335932cc39725c74c1d7b0665997
Daksh Malik <158033486+DakshMalik22@users.noreply.github.com>
2024-04-02 22:54:45 +0530

    Update CountSmallerElements.java

--- 22-Mar-2024/CountSmallerElements.java
+++ 22-Mar-2024/CountSmallerElements.java
@@ -1,89 +1,44 @@
-import java.util.Scanner;
-
+import java.util.Arrays;
 
 public class CountSmallerElements {
 
     public static int[] countSmallerElements(int[] arr) {
         int n = arr.length;
-        int[] result = new int[n];
-        int[] indices = new int[n];
-        int[] temp = new int[n];
-        int[] tempIndices = new int[n];
-        for (int i = 0; i < n; i++) {
-            indices[i] = i;
+        int[] sortedArr = Arrays.copyOf(arr, n);
+        Arrays.sort(sortedArr);
+
+        int[] count = new int[n];
+        int[] bit = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int index = Arrays.binarySearch(sortedArr, arr[i]) + 1;
+            count[i] = query(bit, index - 1);
+            update(bit, index, 1);
         }
-        mergeSort(arr, indices, temp, tempIndices, 0, n - 1, result);
-        return result;
+
+        return count;
     }
 
-    private static void mergeSort(int[] arr, int[] indices, int[] temp, int[] tempIndices, int left, int right, int[] result) {
-        if (left >= right) {
-            return;
+    private static int query(int[] bit, int index) {
+        int sum = 0;
+        while (index > 0) {
+            sum += bit[index];
+            index -= index & (-index);
         }
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, indices, temp, tempIndices, left, mid, result);
-        mergeSort(arr, indices, temp, tempIndices, mid + 1, right, result);
-        merge(arr, indices, temp, tempIndices, left, mid, right, result);
+        return sum;
     }
 
-    private static void merge(int[] arr, int[] indices, int[] temp, int[] tempIndices, int left, int mid, int right, int[] result) {
-        int i = left;
-        int j = mid + 1;
-        int k = left;
-        int count = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[indices[i]] <= arr[indices[j]]) {
-                result[indices[i]] += count;
-                temp[k] = arr[indices[i]];
-                tempIndices[k] = indices[i];
-                k++;
-                i++;
-            } else {
-                count++;
-                temp[k] = arr[indices[j]];
-                tempIndices[k] = indices[j];
-                k++;
-                j++;
-            }
-        }
-
-        while (i <= mid) {
-            result[indices[i]] += count;
-            temp[k] = arr[indices[i]];
-            tempIndices[k] = indices[i];
-            k++;
-            i++;
-        }
-
-        while (j <= right) {
-            temp[k] = arr[indices[j]];
-            tempIndices[k] = indices[j];
-            k++;
-            j++;
-        }
-
-        for (i = left; i <= right; i++) {
-            arr[tempIndices[i]] = temp[i];
-            indices[i] = tempIndices[i];
+    private static void update(int[] bit, int index, int value) {
+        while (index < bit.length) {
+            bit[index] += value;
+            index += index & (-index);
         }
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-
-            int N = scanner.nextInt();
-            int[] arr = new int[N];
-            for (int i = 0; i < N; i++) {
-                arr[i] = scanner.nextInt();
-            }
-            int[] result = countSmallerElements(arr);
-            for (int i = 0; i < N; i++) {
-                System.out.print(result[i] + " ");
-            }
-            System.out.println();
-        }
-
+        int[] arr = {4, 2, 1, 5};
+        int[] result = countSmallerElements(arr);
+        System.out.println(Arrays.toString(result)); // Output: [2, 1, 0, 0]
+    }
 }
 
